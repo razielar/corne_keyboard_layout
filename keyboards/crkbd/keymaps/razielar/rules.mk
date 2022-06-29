a12c9bd457269a961ab7ef3b99897bc0947d5909
razielar <razielar@gmail.com>
2022-06-29 09:48:49 +0200

    put init config

---
+++ keyboards/crkbd/keymaps/razielar/rules.mk
@@ -0,0 +1,7 @@
+RGBLIGHT_ENABLE    = no # Basic RBG
+RGB_MATRIX_ENABLE  = WS2812 # Better RBG
+MOUSEKEY_ENABLE    = no
+NKRO_ENABLE        = yes # default yes
+OLED_DRIVER_ENABLE = yes
+EXTRAKEY_ENABLE = yes # Enable key-combinations
+TAP_DANCE_ENABLE = yes
