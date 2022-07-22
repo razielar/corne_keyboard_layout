1a097112f5081d99ca08e4664b8f0d16b41a79fc
razielar <razielar@gmail.com>
2022-07-22 23:08:18 +0200

    change rules

--- keyboards/crkbd/keymaps/default/rules.mk
+++ keyboards/crkbd/keymaps/default/rules.mk
@@ -1,5 +1,12 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+# RBG:
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE  = yes
+# OLED:
 OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
-LTO_ENABLE      = yes
+# TAP_DANCE:
+TAP_DANCE_ENABLE = yes
+# Not used: 
+MOUSEKEY_ENABLE = no       # Mouse keys
+# EXTRAS:
+LTO_ENABLE      = yes      #enable link time optimization to reduce binary size
