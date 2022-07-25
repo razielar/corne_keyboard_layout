08aeb5b63b4cfab906577461a1dd6a21d34e097e
razielar <razielar@gmail.com>
2022-07-25 18:31:58 +0200

    add prev commits; layer 3 almos done; one extra symbol

--- keyboards/crkbd/keymaps/razielar/keymap.c
+++ keyboards/crkbd/keymaps/razielar/keymap.c
@@ -25,11 +25,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_DEL,
+      KC_LALT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_TAB,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(1),  KC_SPC,     KC_ENT,   MO(2), KC_RALT
+                                          KC_LGUI,   MO(1),  KC_SPC,     KC_ENT,   MO(2), KC_LSFT
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -38,9 +38,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,   XXXXXXX, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_SLEP, XXXXXXX, XXXXXXX, KC_MPLY, KC_VOLU, XXXXXXX,                      KC_PGUP, XXXXXXX,   XXXXXXX,XXXXXXX, KC_UP,  KC_QUOT,
+      KC_SLEP, KC_CAPS, XXXXXXX, KC_MPLY, KC_VOLU, XXXXXXX,                      KC_HOME, KC_PGUP,   XXXXXXX,XXXXXXX, KC_UP,  KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_PWR, XXXXXXX, XXXXXXX, KC_MPRV, KC_VOLD, KC_MNXT,                       KC_PGDN, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,
+      KC_PWR, XXXXXXX, XXXXXXX, KC_MPRV, KC_VOLD, KC_MNXT,                       KC_END, KC_PGDN, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______,  KC_SPC,     KC_ENT,   MO(3), KC_RALT
                                       //`--------------------------'  `--------------------------'
@@ -48,11 +48,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+       KC_PIPE, KC_EXLM,  KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_AMPR, KC_QUES, KC_CIRC, KC_LPRN, KC_RPRN, KC_BSLS,
+
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                                  KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+      KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                                 KC_PLUS,  KC_MINS, KC_UNDS, KC_LBRC, KC_RBRC,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+       KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,                            KC_ASTR,  KC_EQL,  XXXXXXX, KC_LCBR, KC_RCBR,  KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   MO(3),  KC_SPC,     KC_ENT, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
@@ -70,6 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   )
 };
+
 
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
@@ -175,3 +177,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 #endif // OLED_ENABLE
+
+#ifdef RGB_MATRIX_ENABLE
+    // Whe computer is suspended do the same with the keyboard
+    void suspend_power_down_user(void) {
+        rgb_matrix_set_suspend_state(true);
+    }
+    // If the computer is wakeup do the same with the keyboard
+    void suspend_wakeup_init_user(void) {
+        rgb_matrix_set_suspend_state(false);
+    }
+#endif
+
+
