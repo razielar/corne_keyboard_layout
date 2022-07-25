b0fb1c76c33edf73830701be7f90f823e7a901ea
razielar <razielar@gmail.com>
2022-07-25 18:54:02 +0200

    arrows in first layer and sort out highlighting

--- keyboards/crkbd/keymaps/razielar/keymap.c
+++ keyboards/crkbd/keymaps/razielar/keymap.c
@@ -17,6 +17,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
+// to KC_UP, KC_LEFT, KC_DOWN, KC_RGHT,
+// from KC_SCLN, KC_DOT, KC_SLSH, KC_TAB
+
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
@@ -25,9 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LALT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_DEL,
+      KC_LALT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,  KC_UP,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_TAB,
+      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM, KC_LEFT, KC_DOWN, KC_RGHT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   MO(1),  KC_SPC,     KC_ENT,   MO(2), KC_LSFT
                                       //`--------------------------'  `--------------------------'
@@ -38,11 +41,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,   XXXXXXX, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_SLEP, KC_CAPS, XXXXXXX, KC_MPLY, KC_VOLU, XXXXXXX,                      KC_HOME, KC_PGUP,   XXXXXXX,XXXXXXX, KC_UP,  KC_QUOT,
+      KC_SLEP, KC_CAPS, XXXXXXX, KC_MPLY, KC_VOLU, XXXXXXX,                      KC_HOME, KC_PGUP, XXXXXXX,XXXXXXX, KC_SCLN,  KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_PWR, XXXXXXX, XXXXXXX, KC_MPRV, KC_VOLD, KC_MNXT,                       KC_END, KC_PGDN, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,
+       KC_PWR, XXXXXXX, XXXXXXX, KC_MPRV, KC_VOLD, KC_MNXT,                       KC_END, KC_PGDN, XXXXXXX, KC_DOT, KC_SLSH,  KC_TAB, 
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT,   MO(3), KC_RALT
+                                          KC_LGUI, _______,  KC_SPC,     KC_ENT,   MO(3), KC_LSFT
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -55,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,                            KC_ASTR,  KC_EQL,  XXXXXXX, KC_LCBR, KC_RCBR,  KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(3),  KC_SPC,     KC_ENT, _______, KC_RALT
+                                          KC_LGUI,   MO(3),  KC_SPC,     KC_ENT, _______, KC_LSFT
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -67,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
+                                          KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_LSFT
                                       //`--------------------------'  `--------------------------'
   )
 };
